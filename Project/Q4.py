dcf4c017a98ac811daee72debc8fa196f83c281f
TheDisgraced74 <abhaygod2010@gmail.com>
2026-01-11 18:18:58 +0530

    Create Q4.py

---
+++ Project/Q4.py
@@ -0,0 +1,9 @@
+from random import randint as r
+from random import choice as c
+
+my_list = []
+string = ["Bananas","Python","Call Of Duty","Root","Death","Music","GitHub"]
+elements = [c(string),r(1,1000)]
+
+for i in range(1,8):
+    mylist += c(elements)
