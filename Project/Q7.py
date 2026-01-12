7ccf35ca8cbf4c9787f8be0ca121d1ba05717203
TheDisgraced74 <abhaygod2010@gmail.com>
2026-01-12 11:35:58 +0530

    Update Q7.py

--- Project/Q7.py
+++ Project/Q7.py
@@ -4,10 +4,11 @@ if age <= 0 :
     print("Invalid input")
 
 elif age <= 18 :
-    print(f"You are not eligible for voting.\nYou will be in {(18 - age) * 31622400000000} microseconds.")
+    print(f"You are not eligible for voting.\nYou will be in {(18 - age)} years.")
 
 elif age <= 110 :
     print("You are eligible for voting.")
 
 else :
+
     print("Invalid input")
