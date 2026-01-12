8ac1477cea93e7bfa3cdca9fbddd008773dcbcad
TheDisgraced74 <abhaygod2010@gmail.com>
2026-01-12 14:07:31 +0530

    Update Q6.py

---
+++ Project/Q6.py
@@ -0,0 +1,5 @@
+p = float(input("Please input the principal amount : "))
+r = float(input("Please input the rate of interest (without the %) : "))
+t = float(input("Please input the amount of time : "))
+
+print(f"The simple interest is {(p * r * t) / 100}")
