453b55780e71439f76ffb8d39ca7054b0da2b926
TheDisgraced74 <abhaygod2010@gmail.com>
2026-01-12 19:51:26 +0530

    Update Q1.py

--- Project/Q1.py
+++ Project/Q1.py
@@ -3,8 +3,8 @@ n = 1
 print('Given is the series \n \n')
 
 for r in range(1,5):
-  for c in range(r):
-    print(n , end = " ")
-    n += 1
-  print()
+    for c in range(r):
+        print(n , end = " ")
+        n += 1
+    print()
     
